d2127aedf07ee2b1f7761df1105afad3ce43bcc6
minn <minn@ibeky.com>
2024-01-19 04:22:58 +0000

    updated homepage

--- app/page.js
+++ app/page.js
@@ -1,13 +1,50 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.css'
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <p><Link href="/meals">Meals</Link></p>
-      <p><Link href="/meals/share">Share Meal</Link></p>
-      <p><Link href="/community">Community</Link></p>
-    </main>
+    <>
+      <header>
+        <div className='slideshow'></div>
+        <div>
+          <div className='slideshow-text'>
+            <h1>Love a meal? Share a meal.</h1>
+            <p>Taste & share meals from all over the world!</p>
+          </div>
+          <div className='cta'>
+            <Link href="/community">Join Our Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section>
+          <h2>How it works</h2>
+          <p>
+            MEALS OF THE DAY is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            MEALS OF THE DAY is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section>
+          <h2>Why MEALS OF THE DAY?</h2>
+          <p>
+            MEALS OF THE DAY is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            MEALS OF THE DAY is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   )
 }
--- components/header/header.js
+++ components/header/header.js
@@ -1,28 +1,31 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import HeaderBackground from "./header-background";
 import logoImg from '@/assets/logo.png';
-
 import './header.css'
 
 export default function Header() {
     return (
-        <header>
-            <Link href="/">
-                <Image src={logoImg.src} alt="Meals App Logo." width={500} height={500}/>
-                Meals App
-            </Link>
+        <>
+            <HeaderBackground/>
+            <header>
+                <Link href="/" className="logo">
+                    <Image src={logoImg.src} alt="Meals App Logo." width={100} height={100} className="logoImg"/>
+                    MEALS OF THE DAY
+                </Link>
 
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/meals">Browse Meals</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Meals Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="/meals">Browse Meals</Link>
+                        </li>
+                        <li>
+                            <Link href="/community">Meals Community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     )
 }
